305fd2f056c356cf6f8a9a520852c6a804b71573
kaizenx <tzuchjeh@gmail.com>
2022-12-26 23:09:25 +0800

    added fast-api implementation

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import whisper
+import aiofiles
+import time
+import os.path
 
 app = FastAPI()
+model = whisper.load_model('base')
+
+@app.post("/files/")
+async def create_file(file: bytes = File(description="A file read as bytes")):
+    return {"file_size": len(file)}
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/uploadfile/")
+async def create_upload_file( file: UploadFile = File() ):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        results = []
+        try:
+            contents = await file.read()
+            filename = str(int(time.time())) +"-"+ file.filename
+            completeName = os.path.join("uploads/", filename)
+            async with aiofiles.open(completeName, 'wb') as f:
+                await f.write(contents)
+            result = model.transcribe(completeName)
+            results.append({
+                'filename': filename,
+                'transcript': result['text'],
+            })
+        except Exception:
+            return {"message": "There was an error uploading the file"}
+        finally:
+            await file.close()
 
-# import whisper
-
-# model = whisper.load_model("base")
-# result = model.transcribe("audio.mp3")
-# print(result["text"])
+        return {'results': results}
